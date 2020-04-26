aa2266a9e1989deb86cc59974bd1d564bf58e890
Innoman <innoman.noma@gmail.com>
2020-04-26 15:48:39 +0200

    Express code that display single member

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const path = require('path');
-const moment = require('moment');
+const logger = require('./Middleware/logger')
 const members = require('./Members');
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(
-      `${req.protocol}://${req.get('host')}${
-        req.originalUrl
-      }: ${moment().format()}`
-    );
-    next();
-  };
-
 //Init middleware
-app.use(logger);
+//app.use(logger);
 
   //Gets all members
-app.get('/api/members', (req,res) => {
-res.json(members);
+app.get('/api/members', (req,res) => res.json(members));
+
+//Gets a single member
+app.get('/api/members/:id', (req,res) =>{
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
 });
+
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
